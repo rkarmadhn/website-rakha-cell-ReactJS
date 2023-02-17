b7338612edbdc33ac4ba97a8860a3c62d6cc329f
rkarmadhn <rakaaputtra1@gmail.com>
2023-02-17 21:30:43 +0700

    Fix Button

--- src/Components/templates/Home Page/ServiceSection.jsx
+++ src/Components/templates/Home Page/ServiceSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Banner from "../../UI/Atoms/Banner/Banner";
 import Containers from "../../UI/Atoms/Containers/Containers";
 
@@ -8,17 +9,17 @@ export default function ServiceSection() {
       <Banner className="md:w-1/2 w-full banner-android">
         <div className="text-center">
           <h1 className="md:text-4xl text-2xl font-bold text-white">Service Android</h1>
-          <a href="/service/android/samsung" rel="noopener noreferrer" className="text-white">
+          <Link to="/service/android/samsung" className="text-white">
             Selanjutnya
-          </a>
+          </Link>
         </div>
       </Banner>
       <Banner className="md:w-1/2 w-full banner-iphone">
         <div className="text-center">
           <h1 className="md:text-4xl text-2xl font-bold text-white">Service iPhone</h1>
-          <a href="/servic/" target="_blank" rel="noopener noreferrer" className="text-white">
+          <Link target="_blank" rel="noopener noreferrer" className="text-white">
             Selanjutnya
-          </a>
+          </Link>
         </div>
       </Banner>
     </Containers>
