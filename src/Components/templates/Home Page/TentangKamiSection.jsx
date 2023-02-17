0609c12f082f21dd80d4206f55a98ce22225e90a
rkarmadhn <rakaaputtra1@gmail.com>
2023-02-17 20:48:38 +0700

    fix tentang-kami section

--- src/Components/templates/Home Page/TentangKamiSection.jsx
+++ src/Components/templates/Home Page/TentangKamiSection.jsx
@@ -5,7 +5,7 @@ export default function TentangKamiSection() {
   return (
     <Banner className="banner-tentang-kami">
         <div className="text-center">
-          <h1 className="md:text-4xl text-2xl font-bold text-white">Service iPhone</h1>
+          <h1 className="md:text-4xl text-2xl font-bold text-white">Tentang Kami!</h1>
           <a href="http://" target="_blank" rel="noopener noreferrer" className="my-4 text-white">
             Selanjutnya
           </a>
