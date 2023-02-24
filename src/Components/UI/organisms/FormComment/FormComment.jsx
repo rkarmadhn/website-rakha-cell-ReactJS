b6a403670b752026ed4aabb789b0adc4d34e16c7
rkarmadhn <rakaaputtra1@gmail.com>
2023-02-24 18:21:01 +0700

    fix

--- src/Components/UI/organisms/FormComment/FormComment.jsx
+++ src/Components/UI/organisms/FormComment/FormComment.jsx
@@ -4,20 +4,18 @@ import "./FormComment.scss";
 export default function FormComment() {
   return (
     <div className="flex justify-center items-center">
-      <div className="grid md:gap-2 p-6 rounded-lg bg-neutral-300 min-h-[379px] max-h-[379px]">
+      <div className="md:grid md:gap-2 p-6 rounded-lg bg-neutral-300 min-h-[379px] max-h-[379px]">
         <h4 className="text-xl text-center">Review</h4>
         <div className="input-box">
           <input placeholder="Masukan nama anda" className="input" name="name" type="text"/>
-          <span className="input-helper">Masukan nama yang benar</span>
         </div>
         <div className="input-box">
-          <textarea name="body" id="body" cols="30" rows="5" placeholder="Masukan Kritik dan Saran" className="input hidden md:block"></textarea>
-          <textarea name="body" id="body" cols="30" rows="2" placeholder="Masukan Kritik dan Saran" className="input md:hidden"></textarea>
-          <span className="input-helper">Kritik & Saran</span>
+          <textarea name="body" id="body" cols="30" rows="5" placeholder="Masukan Kritik dan Saran" className="input"></textarea>
         </div>
         <button className="btn-cssbuttons">
           <span className="tracking-widest">Kirim</span>
         </button>
+        <p className="my-3 text-md text-neutral-500">Alamat: Jl. Masjid Nurul Ihsan, <br className="md:hidden"/> Jatiwaringin, <br className="md:block hidden"/> Pondok Gede, Kota Bekasi</p>
       </div>
     </div>
   );
--- src/Components/templates/Home Page/CommentSection.jsx
+++ src/Components/templates/Home Page/CommentSection.jsx
@@ -2,29 +2,30 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import CardComment from "../../UI/molecules/CardComment/CardComment";
 import FormComment from "../../UI/organisms/FormComment/FormComment";
+import './CommentSection.scss'
 export default class CommentSection extends Component {
   render() {
     const settings = {
+      dots: true,
       arrows: false,
       autoplay: true,
-      autoplaySpeed: 1000,
+      autoplaySpeed: 2500,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
-      responsive: [
-        { breakpoint: 1024, settings: { slidesToShow: 2, autoplaySpeed: 1500, } },
-        { breakpoint: 768, settings: { slidesToShow: 1, autoplaySpeed: 2000, } },
-      ],
+      responsive: [{ breakpoint: 1024, settings: { slidesToShow: 1, autoplaySpeed: 2500 } }],
     };
     return (
-      <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 py-8">
-        <Slider {...settings} className="lg:col-span-2">
+      <div className=" sm:grid sm:grid-cols-2 lg:grid-cols-3 py-8">
+        <Slider {...settings} className="slider-comment lg:col-span-2">
+          <CardComment />
+          <CardComment />
           <CardComment />
           <CardComment />
           <CardComment />
           <CardComment />
         </Slider>
-        <FormComment/>
+        <FormComment />
       </div>
     );
   }
