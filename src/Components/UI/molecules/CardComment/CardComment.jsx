9dfda2849caec3ee337fe05c2f26253106138cdb
rkarmadhn <rakaaputtra1@gmail.com>
2023-02-24 21:18:51 +0700

    fix

--- src/Components/UI/molecules/CardComment/CardComment.jsx
+++ src/Components/UI/molecules/CardComment/CardComment.jsx
@@ -4,12 +4,12 @@ import "./CardComment.scss";
 export default function CardComment() {
   return (
     <div className="card-comment bg-neutral-300 shadow shadow-neutral-300 m-1">
-        <p className="text-2xl italic text-center">Rakha Putra</p>
-        <p className="card-body-text">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quibusdam animi molestiae lores?
-        </p>
-        <p className="text-center">30 Januari 2023</p>
-      
+      <p className="text-2xl italic text-center">Rakha Putra</p>
+      <p className="card-body-text">
+      {/* 150 huruf */}
+      itaconsectetur quibusdam animi molestiae lores? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quibusdam animi molestiae lores? L
+      </p>
+      <p className="text-center">30 Januari 2023</p>
     </div>
   );
 }
