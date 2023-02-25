487d35f221090193779a536666a95fb944f2563b
rkarmadhn <rakaaputtra1@gmail.com>
2023-02-25 18:08:21 +0700

    fix

--- src/Components/UI/Atoms/is_mobile_view/HomeCard/HomeCard.jsx
+++ src/Components/UI/Atoms/is_mobile_view/HomeCard/HomeCard.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeCard.scss'
 
 export default function HomeCard() {
+  const [isClick, setIsClick] = useState(false)
+  function handleShowText() {
+    setIsClick(!isClick)
+  }
   return (
-    <div className='is_card_banner'>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzqY-LUauRatjhhXPfWpjlgpbiCpM-BhWiLQ&usqp=CAU" alt="" />
-        <div className='is_card_text'>
+    <div className={`is_card_banner ${isClick ? 'is_click' : ''}`}>
+        <img src="https://asset.kompas.com/crops/o7GotfNUVZYZrcmCaQJD1TF5084=/92x0:1105x675/750x500/data/photo/2021/11/19/6197114195885.jpg" alt="Iphone 13 Pro Max" />
+        <div className='is_card_text' onClick={handleShowText}>
             <h3>Iphone 13 Pro Max</h3>
             <div className='is_card_2nd_text'>
                 <p>Harga LCD</p>
                 <p>Rp. 1.000.000,-</p>
             </div>
-            <div className='is_before_card'></div>
         </div>
     </div>
   )
