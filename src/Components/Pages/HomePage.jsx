73f25e64921ab8ea49793ec86ba1507e5e4db758
rkarmadhn <rakaaputtra1@gmail.com>
2023-03-08 11:19:14 +0700

    fix

--- src/Components/Pages/HomePage.jsx
+++ src/Components/Pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useState } from "react";
 import ResponsiveMobile from "../templates/ResponsiveMobile";
-import { HomeSlider } from "../UI/organisms/is_mobile_view/homeSlider/HomeSlider";
+import HomeSlider from "../UI/organisms/is_mobile_view/homeSlider/HomeSlider";
 import RowKategoriIphone from "../UI/organisms/is_mobile_view/rowKategori/RowKategoriIphone";
 import RowKategoriSamsung from "../UI/organisms/is_mobile_view/rowKategori/RowKategoriSamsung";
 
